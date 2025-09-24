913423523e32cf55f42e86e9d842c9e34ecb8659
Alexmary Rangel <alexmaryrangel@gmail.com>
2025-09-24 12:31:14 +0200

    update-info

--- business_info.py
+++ business_info.py
@@ -26,7 +26,12 @@ TecnoLogiStore es un eCommerce especializado para la venta de las ultimas tenden
 4. **Smartwatch FitTrack 3**
     - Precio: $250 USD
     - Descripción: Reloj inteligente con monitor de salud, GPS y notificaciones inteligentes.
-    - Garantía: 1 año    
+    - Garantía: 1 año   
+
+5. **Iphone 17 Pro**
+    - Precio: $1199.99 USD
+    - Descripción: El ultimo modelo de Iphone con mejoras en camara, rendimiento y duracion de bateria.
+    - Garantia: 1 año         
 
 ---
 
@@ -48,13 +53,27 @@ TecnoLogiStore es un eCommerce especializado para la venta de las ultimas tenden
 #### **Contacto**
     -** Atención al cliente:**
     - Horario: Lunes a Viernes de 9:00 AM a 18:00 PM (hora local).
-    - Email: soporte@techtrendstore.com
-    -Teléfono: +1 (555) 123-4567
+    - Email: soporte@tecnoLogiStore.com
+    -Teléfono: +34 (555) 123-4567
 
 **Información adicional:**
     - **Redes Sociales:**
     - Síguenos en Instagram, Facebook y Twitter en nuestra cuenta oficial @TecnoLogiStore para las últimas novedades y promociones.
 
+---
+---
+### ** Venta de productos**
+    - Para realizar compras en TecnologiStore, puede explorar nuestros productos destacados en nuestro sitio web www.tecnologiStore.com y utilizar el carrito de compras para completar su pedido de manera segura.
+    - Ofrecemos una amplia gama de productos electrónicos, incluyendo smartphones, laptops, auriculares y smartwatches.
+    - Todos nuestros productos son nuevos y cuentan con garantía del fabricante.
+    
+---
+
+### ** Promociones**
+    - Ofrecemos descuentos especiales durante eventos como Black Friday, Cyber Monday y Navidad.
+    - Suscríbete a nuestro boletín para recibir ofertas exclusivas y cupones de descuento.
+    - Consulta nuestra sección de promociones en el sitio web para estar al tanto de las últimas ofertas.
+    
 ---
 
 Con esta información, el chatbot podrá responder preguntas sobre productos, políticas de envío y devoluciones, métodos de pago y contacto. Si necesitas más detalles o quieres agregar algo específico, házmelo saber.
